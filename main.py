235f6649e51185a8094e00e87e45036e28e9d288
Christian Panadero <panavtec@gmail.com>
2017-01-25 14:37:57 +0000

    inlined objects_of_branch

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ def lambda_handler(event, context):
     parsed_message = parse_message(event)
     if (is_closed_pr(parsed_message)):
         branch_name = parse_branch_name(parsed_message)
-        print(branch_name)
-        return delete_objects_of_branch(branch_name)
+        paginator = get_bucket_paginator()
+        objects_to_delete = objects_of_branch(paginator, branch_name)
+        delete_objects_of_bucket(get_s3client(), objects_to_delete)
     else:
         return 'PR event is not closed'
+
+def get_bucket_paginator():
+    return boto3.client('s3').get_paginator('list_objects')
+
+def get_s3client():
+    return s3.meta.client
 
 def parse_branch_name(parsed_message):
     ref = parsed_message['ref']
@@ -26,22 +33,13 @@ def parse_message(event):
     parsed_message = json.loads(message)
     return parsed_message
 
-def delete_objects_of_branch(branch_name):
-    paginator = boto3.client('s3').get_paginator('list_objects')
-    objects_to_delete = objects_of_branch(paginator, branch_name)
-    if objects_to_delete:
-        delete_objects_of_bucket(s3.meta.client, objects_to_delete)
-        return 'Deleted %s folder' %branch_name
-    else:
-        return 'Could not delete the folder with name: %s' %branch_name
-
 def objects_of_branch(paginator, branch_name):
     page_iterator = paginator.paginate(Bucket = BUCKET_NAME, Prefix = FOLDER_FORMAT %branch_name)
 
     return reduce(lambda page, acc: page + acc, page_iterator, [])
 
 def extract_object_keys(objects):
-    return [{'Key' : k} for k in [obj['Key'] 
+    return [{'Key' : k} for k in [obj['Key']
         for obj in objects.get('Contents', [])]]
 
 def delete_objects_of_bucket(s3client, keys):
